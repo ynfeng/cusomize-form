aca49d86878aa2d1d029ee9d01ede1244840d68b
ynfeng <fynwin@gmail.com>
2021-09-02 19:01:01 +0800

    完善api

--- src/main/java/com/github/ynfeng/customizeform/publish/http/ApiRepresent.java
+++ src/main/java/com/github/ynfeng/customizeform/publish/http/ApiRepresent.java
@@ -1,7 +1,9 @@
 package com.github.ynfeng.customizeform.publish.http;
 
+import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
 
+@Getter
 public class ApiRepresent extends RepresentationModel<ApiRepresent> {
-
+    private final String name = "自定义表单API";
 }
